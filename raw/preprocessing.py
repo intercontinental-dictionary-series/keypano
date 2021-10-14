94c1432b09421caf91cd6956c3bdc7afadbca326
lingulist <mattis.list@lingulist.de>
2021-10-14 09:36:02 +0200

    add prep

---
+++ raw/preprocessing.py
@@ -0,0 +1,38 @@
+from lingpy import *
+from lingpy.compare.partial import Partial
+from lingrex.borrowing import internal_cognates, external_cognates
+from lingrex.cognates import common_morpheme_cognates
+
+def run(wordlist):
+    
+    internal_cognates(wordlist, ref="autocogids", cluster_method="infomap")
+    common_morpheme_cognates(wordlist, ref="autocogids")
+    external_cognates(wordlist, ref="autoborid", cognates="autocogid")
+
+    wordlist.add_entries("cogids", "autocogids", lambda x: x)
+    wordlist.add_entries("cogid", "autocogid", lambda x: x)
+    wordlist.add_entries("borid", "autoborid", lambda x: x)
+
+    alms = Alignments(wordlist, ref="cogids")
+    alms.align()
+
+    D = {0: [
+        "doculect",
+        "concept",
+        "family",
+        "value", 
+        "form",
+        "tokens",
+        "morphemes",
+        "autocogids",
+        "autocogid",
+        "cogids",
+        "cogid",
+        "autoborid",
+        "borid",
+        "alignment",
+        "note"
+        ]}
+    for idx in alms:
+        D[idx] = [alms[idx, h] for h in D[0]]
+    return D
